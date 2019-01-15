bce9b84e748861a21f8deebbcebcaf0c9f9877b2
AVPletnev <awpletnev@gmail.com>
2019-01-16 01:32:11 +0300

    l

--- gulpfile.js
+++ gulpfile.js
@@ -90,6 +90,7 @@ gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/uikit/dist/uikit.min.js',
+		'app/libs/slick/slick.min.js',
 		'app/libs/particles/dist/particles.min.js',
 		'app/js/common.js', // Always at the end
 		])
