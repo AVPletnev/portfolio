4c6289ef0903cfb4f65dc350175f31846403f910
Alexandr Pletnev <sajkerprog@gmail.com>
2022-05-11 21:46:02 +0300

    movie details

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 // Global style
-import GlobalStyle from './components/GlobalStyle';
+import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 // Pages
-import AboutUs from './pages/AboutUs';
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+// Router
+import { Route, Routes } from "react-router-dom";
+import MovieDetail from "./pages/MovieDetail";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<AboutUs />} exact />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/work" element={<OurWork />} />
+        <Route path="/work/:id" element={<MovieDetail />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/OurWork.js
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+// Images
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
+
+function OurWork() {
+  return (
+    <Work>
+      <Movie>
+        <h2>The athlete</h2>
+        <div className="line"></div>
+        <Link to="/work/the-athlete">
+          <img src={athlete} alt="athlete" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The racer</h2>
+        <div className="line"></div>
+        <Link to="/work/the-racer">
+          <img src={theracer} alt="theracer" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The goodtimes</h2>
+        <div className="line"></div>
+        <Link to="/work/goodtimes">
+          <img src={goodtimes} alt="goodtimes" />
+        </Link>
+      </Movie>
+    </Work>
+  );
+}
+
+const Work = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+  /* background: white; */
+  h2 {
+    padding: 1rem 0rem;
+  }
+`;
+
+const Movie = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background: #ccc;
+    margin-bottom: 3rem;
+  }
+  img {
+      width: 100%;
+      height: 70vh;
+      object-fit: cover;
+  }
+`;
+
+export default OurWork;
